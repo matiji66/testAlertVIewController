155ecaebb8079c96ed7425d814dadf2c5b659dbe
matiji66 <1392324625@qq.com>
2016-03-08 15:13:03 +0800

    Initial Commit

---
+++ testAlertVIewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testAlertVIewController
+//
+//  Created by 尹春风 on 16/3/8.
+//  Copyright © 2016年 尹春风. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testAlertVIewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testAlertVIewController
+//
+//  Created by 尹春风 on 16/3/8.
+//  Copyright © 2016年 尹春风. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
